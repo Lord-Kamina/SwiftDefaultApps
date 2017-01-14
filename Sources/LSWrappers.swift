24271bf69a6774ca2bffd803f14a3645235e5c86
Gregorio Litenstein <g.litenstein@gmail.com>
2017-01-13 23:01:24 -0300

    Add License.

--- Sources/LSWrappers.swift
+++ Sources/LSWrappers.swift
@@ -1,10 +1,11 @@
-//
-//  LSWrappers.swift
-//  SwDefaultApps
-//
-//  Created by Gregorio Litenstein Goldzweig on 1/11/17.
-//
-//
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <g.litenstein@gmail.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return., Gregorio Litenstein.
+ * ----------------------------------------------------------------------------
+ */
 
 import AppKit
 
