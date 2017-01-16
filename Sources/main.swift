4c773b1a45f9863e2d2a8dd61f3cabd42c7cec64
Gregorio Litenstein <g.litenstein@gmail.com>
2017-01-16 17:42:23 -0300

    Update some project settings.

--- Sources/main.swift
+++ Sources/main.swift
@@ -10,12 +10,34 @@
 import Foundation
 import SwiftCLI
 
+let bundle = Bundle.init(url: URL(string:"file:///Applications/iTunes.app/")!)
+
+    
 //var test = CopyHandlerForUTI("public.data", LSRolesMask.all)
 //
 //test = CopyHandlerForUTI("", LSRolesMask.all)
 //
 //let test2 = CopyAllHandlersForUTI("public.png", LSRolesMask.all)
 //let test3 = UTTypeFuncs().copyAllHandlersAsString("public.png", LSRolesMask.all)
+
+LSWrappers.Schemes().setDefaultHandler("+magnet","com.barebones.bbedit")
+
+LSWrappers.Schemes().setDefaultHandler("magnet","http://www.google.com")
+
+LSWrappers.Schemes().setDefaultHandler("magnet","file:/http://www.google.com")
+
+LSWrappers.Schemes().setDefaultHandler("magnet","file://http://www.google.com")
+
+LSWrappers.Schemes().setDefaultHandler("magnet","com.barebones.bbedit")
+
+LSWrappers.Schemes().setDefaultHandler("magnet","/Applications/BBEdit.app")
+
+
+LSWrappers.UTType().setDefaultHandler("magnet","public.html")
+
+LSWrappers.Schemes().setDefaultHandler("magnet","com.barebones.bbedit")
+
+LSWrappers.UTType().setDefaultHandler("public.html","com.barebones.bbedit")
 
 CLI.setup(name:"lsreg")
 
@@ -25,8 +47,6 @@ CLI.debugGo(with: "lsreg getUTIs")
 
 CLI.debugGo(with: "lsreg getSchemes")
 
-CLI.debugGo(with: "lsreg getApps")
-
 CLI.debugGo(with: "lsreg getHandler --UTI public.text")
 
 CLI.debugGo(with: "lsreg getHandler --browser")
@@ -35,9 +55,6 @@ CLI.debugGo(with: "lsreg getHandler --internet")
 
 CLI.debugGo(with: "lsreg getHandler --URL public.jpeg")
 
-CLI.debugGo(with: "lsreg getHandler --what")
-
-CLI.debugGo(with: "lsreg -h")
 //
 CLI.debugGo(with: "lsreg getHandler --URL public.jpeg --all")
 CLI.debugGo(with: "lsreg getHandler --UTI public.png --all")
