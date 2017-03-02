ad997d0c0e572d02179224efb0c514c4c331ba50
Gregorio Litenstein <g.litenstein@gmail.com>
2017-03-02 15:43:21 -0300

    More changes to account for new workspace.

---
+++ Sources/CLI Components/main.swift
@@ -0,0 +1,14 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <g.litenstein@gmail.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return., Gregorio Litenstein.
+ * ----------------------------------------------------------------------------
+ */
+
+import Foundation
+import SwiftCLI
+
+CLI.setup(name:"lsreg", version:"1.0", description:"Utility to retrieve and manipulate default applications in macOS.")
+CLI.register(commands: [ ReadCommand(), GetApps(), GetSchemes(), GetUTIs(), SetCommand() ])
