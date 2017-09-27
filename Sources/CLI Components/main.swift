733478d23c68d0b5c94123b55a8e8aec09c87516
Gregorio Litenstein <g.litenstein@gmail.com>
2017-09-26 22:47:18 -0300

    Fix CLI tool.

--- Sources/CLI Components/main.swift
+++ Sources/CLI Components/main.swift
@@ -11,3 +11,5 @@ import Foundation
 import SwiftCLI
 CLI.setup(name:"lsreg", version:"1.0", description:"Utility to retrieve and manipulate default applications in macOS.")
 CLI.register(commands: [ ReadCommand(), GetApps(), GetSchemes(), GetUTIs(), SetCommand() ])
+
+exit(CLI.go())
